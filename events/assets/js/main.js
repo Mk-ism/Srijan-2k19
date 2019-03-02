71626d6beeb05390914fb7c59bcfd87934d7a755
mj.16je002475 <mj.16je002475@agp.ism.in>
2019-03-03 05:08:03 +0530

    Minor fixes. Added temp events.

---
+++ events/assets/js/main.js
@@ -0,0 +1,56 @@
+// Hello.
+//
+// This is The Scripts used for ___________ Theme
+//
+//
+$(window).load(function(){
+    $(".wrapper").fadeOut();
+    $("#content").css({
+        "display":"block",
+    });
+});
+
+function main() {
+
+(function () {
+   'use strict';
+
+   /* ==============================================
+
+  	/*====================================
+    Portfolio Isotope Filter
+    ======================================*/
+    $(window).load(function() {
+        var $container = $('#lightbox');
+        $container.isotope({
+            filter: '*',
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        $('.cat a').click(function() {
+            $('.cat .active').removeClass('active');
+            $(this).addClass('active');
+            var selector = $(this).attr('data-filter');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+            });
+            return false;
+        });
+
+    });
+
+
+
+}());
+
+
+}
+main();
